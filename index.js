2001d14edfdb8671d842e73cbd4ae8426dcf6478
YaroShkvorets <shkvorets@gmail.com>
2020-12-17 19:15:08 -0500

    async fetch prices

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { timeout, getSecMs } = require('./src/utils.js');
+const { timeout, getTime } = require('./src/utils.js');
 const { getBinancePrice } = require('./src/binance.js');
 const { getDefiboxPrice } = require('./src/eos.js');
 
@@ -6,11 +6,15 @@ const { getDefiboxPrice } = require('./src/eos.js');
 (async () => {
     console.log("    Time       Binance      Defibox      Diff")
     while(true) {
-        const binance_price = await getBinancePrice();
-        const eos_price = await getDefiboxPrice();
-        const diff = 100 * (binance_price - eos_price) / binance_price;
 
-        console.log(`${getSecMs()} |  ${binance_price.toFixed(4)}   |   ${eos_price.toFixed(4)}  |   ${diff.toFixed(2)}%  |`);
+        await Promise.all([ getBinancePrice(), getDefiboxPrice() ])
+            .then(([binance_price, defibox_price]) => {
+
+                const diff = 100 * (binance_price - defibox_price) / binance_price;
+                console.log(`${getTime()} |  ${binance_price.toFixed(4)}   |   ${defibox_price.toFixed(4)}  |   ${diff.toFixed(2)}%  |`);
+            })
+            .catch(err => console.log("Failed to fetch prices"));
+
         await timeout(1000);
     }
 })();
--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,7 @@ exports.timeout = function( ms ) {
     return new Promise((resolve) => setTimeout(() => resolve(), ms ))
 }
 
-exports.getSecMs = function( ) {
+exports.getTime = function( ) {
     var d = new Date();
 
     return ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2) + ":" + ("0" + d.getSeconds()).slice(-2) + "." + ("00" + d.getMilliseconds()).slice(-3);
